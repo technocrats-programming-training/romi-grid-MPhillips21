92b50e5997ed6045452e6ada9b55ea80dd28f6e0
MPhillips21 <86633797+MPhillips21@users.noreply.github.com>
2022-07-07 23:00:44 +0000

    Revenge

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,3 +1,4 @@
+/kill @e[type = Gremlin];
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILi.b BSD license file in the root directory of this project.
