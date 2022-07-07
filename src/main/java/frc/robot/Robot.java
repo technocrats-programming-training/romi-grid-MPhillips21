5becd50b635f467d5e464bbf7a27221a581b5978
MPhillips21 <86633797+MPhillips21@users.noreply.github.com>
2022-07-07 22:59:18 +0000

    This better work or

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import lib.ProceduralRobot;
+//import frc.robot.Position;
 
 import java.util.Scanner;
 
@@ -15,8 +16,29 @@ import static lib.Romi.*;
 
 
 public class Robot extends ProceduralRobot {
+  Position n = new Position();
+  Joystick e = new Joystick(0);
   @Override
   public void autonomousProcedure() throws InterruptedException {
+    while(true) {
+      if(e.getRawButtonPressed(1)) {
+        n.driveUp();
+      } else if(e.getRawButtonPressed(2)) {
+        n.driveDown();
+      } else if(e.getRawButtonPressed(3)) {
+        n.driveLeft();
+      } else if(e.getRawButtonPressed(4)) {
+        n.driveRight();
+      } else if(e.getRawButtonPressed(5)) {
+        n.returnToHome();
+        break;
+      } else {
+        System.out.println("no");
+      }
+
     
+    }
+
+
   }
 }
